056a939d5f58330dfe52bb5ceb0fc253e0bc883e
A.Shcherbakov <alex.shcherbakov.as@gmail.com>
2014-10-04 12:31:37 +0400

    Realization of put and get

---
+++ lib.h
@@ -0,0 +1,53 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+/* check `man dbopen` */
+
+enum {MAGIC_CONST = 3};
+
+struct DBT {
+     void  *data;
+     size_t size;
+};
+
+struct DBC {
+        /* Maximum on-disk file size */
+        /* 512MB by default */
+        size_t db_size;
+        /* Maximum page (node/data chunk) size */
+        /* 4KB by default */
+        size_t page_size;
+        /* Maximum memory size */
+        /* 16MB by default */
+        size_t mem_size;
+};
+
+struct DB{
+    /* Public API */
+    int (*close)(const struct DB *db);
+    int (*del)(const struct DB *db, const struct DBT *key);
+    int (*get)(const struct DB *db, struct DBT *key, struct DBT *data);
+    int (*put)(const struct DB *db, struct DBT *key, const struct DBT *data);
+    int (*sync)(const struct DB *db);
+    /* Private API */
+    struct DBC conf;
+    struct DBT dbt;
+    struct BT_node *root;
+    FILE *fl;
+}; /* Need for supporting multiple backends (HASH/BTREE) */
+
+struct DB *dbcreate(const char *file, const struct DBC conf);
+struct DB *dbopen  (const char *file); /* Metadata in file */
+
+struct BT_node {
+    int isleaf;
+    int size;
+    struct BT_key *keys;
+    struct BT_node **lnk;
+    int t;
+};
+
+struct BT_key {
+    struct DBT key;
+    struct DBT val;
+};
